f9529d616c609fc13f1572f5136fac757fae7ab9
Kapil1717 <67622458+Kapil1717@users.noreply.github.com>
2020-09-05 22:02:10 +0530

    Create Swapping.py

---
+++ Swapping.py
@@ -0,0 +1,9 @@
+#121910313016
+#Swapping two numbers
+a=int(input("Enter a number : "))
+b=int(input("Enter a number : "))
+print("Before swapping:",a,b)
+temp=a
+a=b
+b=temp
+print("After swapping:",a,b)
