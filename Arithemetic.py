7ec97c03b3c62716d9cd42063192e72e7860d834
Kapil1717 <67622458+Kapil1717@users.noreply.github.com>
2020-09-05 22:08:32 +0530

    Create Arithemetic.py

---
+++ Arithemetic.py
@@ -0,0 +1,13 @@
+#121910313016
+
+# Taking input from the user
+a=int(input("Enter a number:")) 
+b=int(input("Enter another number:")) 
+
+# Performing all arithematic operations
+print("Sum of the numbers is:",a+b) #Addition
+print("Difference of two numbers is:",a-b) #Difference
+print("Product of two numbers is:",a*b) #Multiplication
+print("True division of two numbers is:",a/b) #True division
+print("Modulus of two numbers is:",a%b) #Modulus
+print("Integer division of two numbers is",a//b) #Integer divison
