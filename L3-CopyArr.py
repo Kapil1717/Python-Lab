9335331f04444e45fc71ecae9520adbde32b7d20
Kapil1717 <67622458+Kapil1717@users.noreply.github.com>
2020-09-08 10:13:03 +0530

    Create L3-CopyArr.py

---
+++ L3-CopyArr.py
@@ -0,0 +1,26 @@
+#121910313016
+
+#Taking the elements of the array from the user 
+n1 = int(input("Enter the size of the original array:"))
+a=[None] * n1
+print("Enter the elements of the array : ")
+for i in range(0,n1):
+    a[i] = int(input()) 
+
+
+#Creating another array with the size of first array
+b = [None] * n1
+
+
+#Copying all the elements from a to b
+for i in range(0,n1):
+    b[i] = a[i]
+
+#Displaying the elements of the original array
+print("Elements of the original array is :")
+print(a)
+
+#Displaying the elements of the duplicate array
+print("Elements of the duplicate array is :")
+print(b)
+    
