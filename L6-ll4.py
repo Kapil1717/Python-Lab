404f25cdfdfa59f395dc85ca0cb74abe0cd40f80
Kapil Sarma <Sarmakapil1817@gmail.com>
2020-10-24 01:51:58 +0530

    Update L6-ll4.py

--- L6-ll4.py
+++ L6-ll4.py
@@ -1,13 +1,15 @@
 #121910313016
-#creating a class
+
 class Node:
     def __init__(self,data):
         self.data=data
         self.next=None
+        
 class LinkedList:
     def __init__(self):
         self.head=None
-    def append(self,data):
+        
+    def add(self,data):
         new_node=Node(data)
         if self.head is None:
             self.head=new_node
@@ -17,15 +19,16 @@ class LinkedList:
             while temp.next:
                 temp=temp.next
             temp.next=new_node
+            
     def print_list(self):
         temp=self.head
         while temp:
             print(temp.data)
             temp=temp.next
 
-l=LinkedList()
-n=int(input("Enter how many numbers you want to add "))
+l = LinkedList()
+n = int(input("Enter how many numbers you wanna add "))
 for i in range(n):
-    data=int(input("Enter the data item: "))
-    l.append(data)
+    data=int(input("Enter the item: "))
+    l.add(data)
 l.print_list()
