10ad9dca9441d448389671034c0a50d2e7367c00
Kapil Sarma <Sarmakapil1817@gmail.com>
2020-10-27 11:21:46 +0530

    Create L9-stack_list.py

---
+++ L9-stack_list.py
@@ -0,0 +1,30 @@
+class stack:
+
+    def __init__(self):
+        self.items = []
+
+    def push(self,item):
+        self.items.append(item)
+
+    def pop(self,data):
+        self.items.pop()
+
+    def peek(self):
+        print(self.items[len(self.items)-1])
+        
+    def printStack(self):
+        print(self.items)
+
+st = stack()
+n = int(input("Enter the number of elements you want to push : "))
+print("Enter the elements : ")
+for i in range(n):
+    a = input()
+    st.push(a)    
+print("Topmost element in the stack is (peek) : ")
+st.peek()
+print("Stack before popping is : ")
+st.printStack()
+st.pop()
+print("Stack after popping is : " )
+st.printStack()    
