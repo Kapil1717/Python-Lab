df03867d942f67403f028d4696aed783fdffc245
Kapil Sarma <Sarmakapil1817@gmail.com>
2020-11-10 11:00:52 +0530

    Update L11-parenthesis.py

--- L11-parenthesis.py
+++ L11-parenthesis.py
@@ -5,26 +5,34 @@ class stack:
     def push(self,item):
         self.items.append(item)
     def pop(self):
-        self.items.pop()
+        return self.items.pop()
     def isempty(self):
         return self.items == []
+
 def par_checker(symbol_string):
     s = stack()
     balanced = True
     i = 0
     while i < len(symbol_string) and balanced:
         symbol = symbol_string[i]
-        if symbol ==  "(":
+        if symbol in "([{":
             s.push(symbol)
         else:
             if s.isempty():
                 balanced = False
             else:
-                s.pop()
+                top = s.pop()
+                if not matches(top,symbol):
+                    balanced = False
         i = i + 1
     if(balanced and s.isempty()):
         return True
     else:
         return False
-print(par_checker('()()()(())'))
-print(par_checker('(())()(()'))
+def matches(open, close):
+    opens = "([{"
+    closes = ")]}"
+    return opens.index(open) == closes.index(close)
+
+print(par_checker('([{}])'))        #Balanced
+print(par_checker('({}([])'))       #Unbalanced
